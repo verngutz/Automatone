fb5c8ad05574d768aa37f0b7679de5f7af137713
munsu <ranupol@gmail.com>
2011-05-16 16:42:42 +0800

    various shit convenience shit methods

--- Chord.java
+++ Chord.java
@@ -10,9 +10,27 @@ public class Chord
 		third = keyboard[(base - 'a' + 2) % 7];
 		fifth = keyboard[(base - 'a' + 4) % 7];
 	}
+	public String randomVolume(double lbound, double ubound)
+	{
+		return " $" + (Math.random() * (ubound - lbound) + lbound) + ";\n";
+	}
 	public String toString()
 	{
-		return "\t+" + base + "3 $7F;\n\t+" + third + "3 $7F;\n\t+" + fifth + "3 $7F;\n" 
-		+ "\t1/2;-" + base + "3 $00;\n\t-" + third + "3 $00;\n\t-" + fifth + "3 $00;\n";
+		String vol = randomVolume(100.0,127.0);
+		String octave = "3";
+		String time = "1/2";
+		return strum(time, octave, vol) + strum(time, octave);
+	}
+	public String strum(String time, String octave, String vol)
+	{
+		return 	"\t" + time + ";+" + base + octave + vol + 
+				"\t+" + third + octave + vol + 
+				"\t+" + fifth + octave + vol;
+	}
+	public String strum(String time, String octave)
+	{
+		return 	"\t" + time + ";-" + base + octave + " $00;\n" +
+				"\t-" + third + octave + " $00;\n" +
+				"\t-" + fifth + octave + " $00;\n";
 	}
 }
