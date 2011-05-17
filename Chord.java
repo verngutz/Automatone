ca69b95a7c6e282eb86e75871dbd79bde7faffbe
Vernon G <vern_go_123@yahoo.com>
2011-05-17 18:05:49 +0800

    Work to be continued somewhere else.

--- Chord.java
+++ Chord.java
@@ -1,11 +1,20 @@
 public class Chord
 {
+	public enum Mode
+	{
+		MAJOR,
+		MINOR,
+		AUGMENTED,
+		DIMINISHED
+	}
 	
 	private NoteName base;
 	private Mode mode;
-	private ArrayList<NoteName> chordConstituents;
+	private ArrayList<NoteName> harmoniousNotes;
+	private ArrayList<NoteName> neutralNotes;
+	private ArrayList<NoteName> dissonantNotes;
 	public Chord(NoteName base, Mode mode)
 	{
-		this.chordConstituents = chordConstituents;
+		
 	}
 }
