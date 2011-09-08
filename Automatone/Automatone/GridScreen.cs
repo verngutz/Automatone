3e56d32cfe4b5c32cf2fa2f477709281cb086dbe
Vernon G <vern_go_123@yahoo.com>
2011-09-08 22:31:22 +0800

    Removed MoodSwing Framework and integrated Nuclex Framework

---
+++ Automatone/Automatone/GridScreen.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Automatone
+{
+    public class GridScreen : DrawableGameComponent
+    {
+        private Automatone automatone;
+
+        private Rectangle boundingRectangle;
+
+        public Vector2 gridOffset;
+        public float playOffset;
+
+        public const byte CELLSIZE = 12;
+
+        private static Texture2D silentCell;
+        private static Texture2D startCell;
+        private static Texture2D holdCell;
+
+        public bool ScrollWithMidi { set; get; }
+
+        public const int GRID_Y_POSITION = 150;
+
+        private float moveValX = 0;
+        private float moveValY = 0;
+
+        private const float INIT_GRID_MOVE_SPEED = 1;
+        private const float GRID_MOVE_ACCELERATION = .02F;
+        private const float MAX_GRID_MOVE_SPEED = 10;
+
+        public GridScreen(Automatone parent)
+            : base(parent)
+        {
+            automatone = parent;
+            boundingRectangle = new Rectangle(0, Automatone.CONTROLS_AND_GRID_DIVISION, Automatone.SCREEN_WIDTH, Automatone.SCREEN_HEIGHT - Automatone.CONTROLS_AND_GRID_DIVISION);
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            Reset();
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+
+            silentCell = automatone.Content.Load<Texture2D>("darkbox");
+            startCell = automatone.Content.Load<Texture2D>("lightbox");
+            holdCell = automatone.Content.Load<Texture2D>("holdbox");
+        }
+
+        protected override void UnloadContent()
+        {
+            silentCell.Dispose();
+            startCell.Dispose();
+            holdCell.Dispose();
+            base.UnloadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (automatone.SongCells != null)
+            {
+                KeyboardState newKeyboardState = Keyboard.GetState();
+
+                if (ScrollWithMidi)
+                {
+                    playOffset -= Automatone.TEMPO * Automatone.SUBBEATS_PER_MEASURE / 14400.0f * CELLSIZE;
+                    gridOffset.X = Math.Min(playOffset + 100, 0);
+                }
+                else
+                {
+                    if (newKeyboardState.IsKeyDown(Keys.Left) == newKeyboardState.IsKeyDown(Keys.Right))
+                    {
+                        moveValX = INIT_GRID_MOVE_SPEED;
+                    }
+                    else if (newKeyboardState.IsKeyDown(Keys.Left))
+                    {
+                        moveValX += GRID_MOVE_ACCELERATION;
+                        if (moveValX > MAX_GRID_MOVE_SPEED)
+                        {
+                            moveValX = MAX_GRID_MOVE_SPEED;
+                        }
+
+                        gridOffset.X += moveValX;
+                    }
+                    else if (newKeyboardState.IsKeyDown(Keys.Right))
+                    {
+                        moveValX += GRID_MOVE_ACCELERATION;
+                        if (moveValX > MAX_GRID_MOVE_SPEED)
+                        {
+                            moveValX = MAX_GRID_MOVE_SPEED;
+                        }
+
+                        gridOffset.X -= moveValX;
+                    }
+                }
+
+                if (newKeyboardState.IsKeyDown(Keys.Up) == newKeyboardState.IsKeyDown(Keys.Down))
+                {
+                    moveValY = INIT_GRID_MOVE_SPEED;
+                }
+                else if (newKeyboardState.IsKeyDown(Keys.Up))
+                {
+                    moveValY += GRID_MOVE_ACCELERATION;
+                    if (moveValY > MAX_GRID_MOVE_SPEED)
+                    {
+                        moveValY = MAX_GRID_MOVE_SPEED;
+                    }
+
+                    gridOffset.Y += moveValY;
+                }
+                else if (newKeyboardState.IsKeyDown(Keys.Down))
+                {
+                    moveValY += GRID_MOVE_ACCELERATION;
+                    if (moveValY > MAX_GRID_MOVE_SPEED)
+                    {
+                        moveValY = MAX_GRID_MOVE_SPEED;
+                    }
+
+                    gridOffset.Y -= moveValY;
+                }
+
+                gridOffset.X = MathHelper.Clamp(gridOffset.X, Automatone.SCREEN_WIDTH - ((automatone.SongCells.GetLength(1) + 1) * CELLSIZE), 0);
+                gridOffset.Y = MathHelper.Clamp(gridOffset.Y, Automatone.SCREEN_HEIGHT + Automatone.CONTROLS_AND_GRID_DIVISION - ((automatone.SongCells.GetLength(0) + 1) * CELLSIZE), 0);
+
+            }
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            if (automatone.SongCells != null)
+            {
+                automatone.SpriteBatch.Begin();
+                for (int i = 0; i < automatone.SongCells.GetLength(0); i++)
+                {
+                    for (int j = 0; j < automatone.SongCells.GetLength(1); j++)
+                    {
+                        Rectangle drawRectangle = new Rectangle((int)(i * CELLSIZE + gridOffset.X), (int)(j * CELLSIZE + gridOffset.Y), CELLSIZE, CELLSIZE);
+                        if (boundingRectangle.Intersects(drawRectangle))
+                        {
+                            Texture2D cellTexture = null;
+                            switch (automatone.SongCells[i, j])
+                            {
+                                case CellState.SILENT:
+                                    cellTexture = silentCell;
+                                    break;
+                                case CellState.START:
+                                    cellTexture = startCell;
+                                    break;
+                                case CellState.HOLD:
+                                    cellTexture = holdCell;
+                                    break;
+                            }
+
+                            if (Math.Abs(drawRectangle.X) < -playOffset - CELLSIZE)
+                            {
+                                automatone.SpriteBatch.Draw(cellTexture, drawRectangle, Color.Gray);
+                            }
+                            else if (Math.Abs(drawRectangle.X) < -playOffset)
+                            {
+                                automatone.SpriteBatch.Draw(cellTexture, drawRectangle, Color.SpringGreen);
+                            }
+                            else
+                            {
+                                automatone.SpriteBatch.Draw(cellTexture, drawRectangle, Color.White);
+                            }
+                        }
+                    }
+                }
+                automatone.SpriteBatch.End();
+            }
+            base.Draw(gameTime);
+        }
+
+        public void Reset()
+        {
+            gridOffset = Vector2.Zero;
+            playOffset = 0;
+        }
+    }
+}
--- Automatone/Automatone/MainScreen.cs
+++ Automatone/Automatone/MainScreen.cs
@@ -1,354 +1,115 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿#define USESEED
 
-using MoodSwingGUI;
-using MoodSwingCoreComponents;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Collections.Generic;
 
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
+using Nuclex.UserInterface;
+using Nuclex.UserInterface.Controls.Desktop;
+
 namespace Automatone
 {
-    public class MainScreen : MSScreen
+    public class MainScreen : WindowControl
     {
-        private MSButton randomButton;
-        private MSTabbedPanel inputTabs;
-        private MSCheckbox playButton;
-        private MSButton rewindButton;
-        private MSButton forwardButton;
-        private MSImageHolder slider;
-        private MSImageHolder sliderCursor;
-        private MSPanel topLeftPanel;
-        private MSPanel topRightPanel;
-        private MSPanel topPanel;
-        public MSPanel gridPanel;
-        public Vector2 gridOffset;
+        private Automatone automatone;
 
-        public float playOffset;
+        private const int SEED = 40;
+        private const int TEMPO_DIVIDEND = 60000000;
 
-        public int gridWidth = 0;
-        public int gridHeight = 0;
-
-        public const int CELLSIZE = 12;
-        public const int GRID_Y_POSITION = 150;
-
-        private float moveValX = 0;
-        private float moveValY = 0;
-
-        private const float INIT_GRID_MOVE_SPEED = 1;
-        private const float GRID_MOVE_ACCELERATION = .02F;
-        private const float MAX_GRID_MOVE_SPEED = 10;
-
-        public bool ScrollWithMidi { set; get; }
-
-        private Dictionary<Vector2, MSButton> gridAccessor;
-
-        public MainScreen(Texture2D background, SpriteBatch spriteBatch, Game game, GraphicsDeviceManager graphics)
-            : this(background, 0, 0, 0, 0, Color.White, spriteBatch, game, graphics) { }
-        
-        public MainScreen(Texture2D background, Color highlight, SpriteBatch spriteBatch, Game game, GraphicsDeviceManager graphics)
-            : this(background, 0, 0, 0, 0, highlight, spriteBatch, game, graphics) { }
-        
-        public MainScreen(Texture2D background, float topPadding, float bottomPadding, float leftPadding, float rightPadding, SpriteBatch spriteBatch, Game game, GraphicsDeviceManager graphics)
-            : this(background, topPadding, bottomPadding, leftPadding, rightPadding, Color.White, spriteBatch, game, graphics) { }
-        
-        public MainScreen(Texture2D background, float topPadding, float bottomPadding, float leftPadding, float rightPadding, Color highlight, SpriteBatch spriteBatch, Game game, GraphicsDeviceManager graphics)
-            : base(background, topPadding, bottomPadding, leftPadding, rightPadding, highlight, spriteBatch, game)
+        public MainScreen(Automatone parent) : base()
         {
-            randomButton = new MSButton(null, new SongRandomizer(), new Rectangle(0, 0, 117, 45),
-                game.Content.Load<Texture2D>("random"),
-                game.Content.Load<Texture2D>("random"),
-                game.Content.Load<Texture2D>("random"),
-                null,
-                Shape.RECTANGULAR,
-                spriteBatch,
-                game);
-
-            playButton = new MSCheckbox(
-                new MSButton(
-                    null, new PlayMidi(), new Rectangle(0, 0, 45, 42),
-                    game.Content.Load<Texture2D>("play"),
-                    game.Content.Load<Texture2D>("play"),
-                    game.Content.Load<Texture2D>("play"),
-                    null,
-                    Shape.RECTANGULAR,
-                    spriteBatch,
-                    game),
-                new MSButton(
-                    null, new PauseMidi(), new Rectangle(0, 0, 45, 42),
-                    game.Content.Load<Texture2D>("stop"),
-                    game.Content.Load<Texture2D>("stop"),
-                    game.Content.Load<Texture2D>("stop"),
-                    null,
-                    Shape.RECTANGULAR,
-                    spriteBatch,
-                    game),
-                false);
-
-            rewindButton = new MSButton(null, null, new Rectangle(0, 0, 33, 36),
-                game.Content.Load<Texture2D>("rev"),
-                game.Content.Load<Texture2D>("rev"),
-                game.Content.Load<Texture2D>("rev"),
-                null,
-                Shape.RECTANGULAR,
-                spriteBatch,
-                game);
-
-            forwardButton = new MSButton(null, null, new Rectangle(0, 0, 33, 36),
-                game.Content.Load<Texture2D>("fwd"),
-                game.Content.Load<Texture2D>("fwd"),
-                game.Content.Load<Texture2D>("fwd"),
-                null,
-                Shape.RECTANGULAR,
-                spriteBatch,
-                game);
-
-            slider = new MSImageHolder(new Rectangle(0, 0, 101, 13), game.Content.Load<Texture2D>("slider"), spriteBatch, game);
-            sliderCursor = new MSImageHolder(new Rectangle(0, 0, 15, 15), game.Content.Load<Texture2D>("slidercursor"), spriteBatch, game);
-
-            topLeftPanel = new MSPanel(null, new Rectangle(0, 0, 400, 150), null, Shape.RECTANGULAR, spriteBatch, game);
-
-            MSPanel basicPanel = new MSPanel(null, new Rectangle(0, 45, 30 * CELLSIZE, 105), 6, 6, 25, 15, null, Shape.RECTANGULAR, spriteBatch, game);
-            basicPanel.AddComponent(new MSWrappingLabel(new Point(0, 0), "Mood:",  game.Content.Load<SpriteFont>("Temp"), Color.White, null, null, null, spriteBatch, game), Alignment.TOP_LEFT);
-            basicPanel.AddComponent(new MSButton(
-                new MSWrappingLabel(new Point(50, 0), "Happy", game.Content.Load<SpriteFont>("Temp"), Color.White, null, null, null, spriteBatch, game),
-                null,
-                new Rectangle(0, 0, 50, 35),
-                game.Content.Load<Texture2D>("radiobutton"),
-                game.Content.Load<Texture2D>("radiobutton"),
-                game.Content.Load<Texture2D>("radiobutton"),
-                null,
-                Shape.RECTANGULAR,
-                spriteBatch,
-                game), Alignment.MIDDLE_LEFT);
-            basicPanel.AddComponent(new MSButton(
-                new MSWrappingLabel(new Point(50, 0), "Sad", game.Content.Load<SpriteFont>("Temp"), Color.White, null, null, null, spriteBatch, game),
-                null,
-                new Rectangle(0, 0, 33, 30),
-                game.Content.Load<Texture2D>("radiobuttonempty"),
-                game.Content.Load<Texture2D>("radiobuttonempty"),
-                game.Content.Load<Texture2D>("radiobuttonempty"),
-                null,
-                Shape.RECTANGULAR,
-                spriteBatch,
-                game), Alignment.BOTTOM_LEFT);
-
-            basicPanel.AddComponent(new MSWrappingLabel(new Point(0, 0), "Speed:", game.Content.Load<SpriteFont>("Temp"), Color.White, null, null, null, spriteBatch, game), Alignment.TOP_CENTER);
-            basicPanel.AddComponent(new MSButton(
-                new MSWrappingLabel(new Point(50, 0), "Fast", game.Content.Load<SpriteFont>("Temp"), Color.White, null, null, null, spriteBatch, game),
-                null,
-                new Rectangle(0, 0, 33, 30),
-                game.Content.Load<Texture2D>("radiobuttonempty"),
-                game.Content.Load<Texture2D>("radiobuttonempty"),
-                game.Content.Load<Texture2D>("radiobuttonempty"),
-                null,
-                Shape.RECTANGULAR,
-                spriteBatch,
-                game), Alignment.MIDDLE_CENTER);
-            basicPanel.AddComponent(new MSButton(
-                new MSWrappingLabel(new Point(50, 0), "Slow", game.Content.Load<SpriteFont>("Temp"), Color.White, null, null, null, spriteBatch, game),
-                null,
-                new Rectangle(0, 0, 50, 35),
-                game.Content.Load<Texture2D>("radiobutton"),
-                game.Content.Load<Texture2D>("radiobutton"),
-                game.Content.Load<Texture2D>("radiobutton"),
-                null,
-                Shape.RECTANGULAR,
-                spriteBatch,
-                game), Alignment.BOTTOM_CENTER);
-
-            MSPanel expertPanel = new MSPanel(null, new Rectangle(), null, Shape.RECTANGULAR, spriteBatch, game);
-
-            inputTabs = new MSTabbedPanel(topLeftPanel);
-            inputTabs.AddTab(new MSTab(
-                new MSButton(
-                    new MSWrappingLabel(new Point(10, 10), "Basic", game.Content.Load<SpriteFont>("Temp"), Color.LightBlue, null, null, null, spriteBatch, game),
-                    null,
-                    new Rectangle(0, 0, 117, 45),
-                    game.Content.Load<Texture2D>("tab"),
-                    game.Content.Load<Texture2D>("tab"),
-                    game.Content.Load<Texture2D>("tab"),
-                    null,
-                    Shape.RECTANGULAR,
-                    spriteBatch,
-                    game),
-                new MSButton(
-                    new MSWrappingLabel(new Point(10, 10), "Basic", game.Content.Load<SpriteFont>("Temp"), Color.LightBlue, null, null, null, spriteBatch, game),
-                    null,
-                    new Rectangle(0, 0, 117, 45),
-                    game.Content.Load<Texture2D>("tab"),
-                    game.Content.Load<Texture2D>("tab"),
-                    game.Content.Load<Texture2D>("tab"),
-                    null,
-                    Shape.RECTANGULAR,
-                    spriteBatch,
-                    game),
-                true,
-                basicPanel));
-
-            inputTabs.AddTab(new MSTab(
-                new MSButton(
-                    new MSWrappingLabel(new Point(10, 10), "Expert", game.Content.Load<SpriteFont>("Temp"), Color.LightBlue, null, null, null, spriteBatch, game),
-                    null,
-                    new Rectangle(117, 0, 117, 45),
-                    game.Content.Load<Texture2D>("tab"),
-                    game.Content.Load<Texture2D>("tab"),
-                    game.Content.Load<Texture2D>("tab"),
-                    null,
-                    Shape.RECTANGULAR,
-                    spriteBatch,
-                    game),
-                new MSButton(
-                    new MSWrappingLabel(new Point(10, 10), "Expert", game.Content.Load<SpriteFont>("Temp"), Color.LightBlue, null, null, null, spriteBatch, game),
-                    null,
-                    new Rectangle(117, 0, 117, 45),
-                    game.Content.Load<Texture2D>("tab"),
-                    game.Content.Load<Texture2D>("tab"),
-                    game.Content.Load<Texture2D>("tab"),
-                    null,
-                    Shape.RECTANGULAR,
-                    spriteBatch,
-                    game),
-                false,
-                expertPanel));
-
-            topLeftPanel.AddComponent(basicPanel);
-
-
-            topRightPanel = new MSPanel(null, new Rectangle(400, 0, 400, 150), 10, 5, 85, 85, null, Shape.RECTANGULAR, spriteBatch, game);
-
-            topRightPanel.AddComponent(randomButton, Alignment.TOP_CENTER);
-            topRightPanel.AddComponent(playButton, Alignment.MIDDLE_CENTER);
-            topRightPanel.AddComponent(rewindButton, Alignment.MIDDLE_LEFT);
-            topRightPanel.AddComponent(forwardButton, Alignment.MIDDLE_RIGHT);
-            topRightPanel.AddComponent(slider, Alignment.BOTTOM_CENTER);
-            topRightPanel.AddComponent(sliderCursor, Alignment.BOTTOM_CENTER);
-
-            topPanel = new MSPanel(null, new Rectangle(0, 0, 800, 150), null, Shape.RECTANGULAR, spriteBatch, game);
-            topPanel.AddComponent(topLeftPanel);
-            topPanel.AddComponent(topRightPanel);
-            
-            AddComponent(topPanel);
-
-            gridPanel = new MSPanel(null, new Rectangle(0, GRID_Y_POSITION, 800, 600), null, Shape.RECTANGULAR, spriteBatch, game);
-            gridOffset = new Vector2(0, 0);
-            gridAccessor = new Dictionary<Vector2, MSButton>();
+            automatone = parent;
+            InitializeComponent();
         }
 
-        public override void Update(GameTime gameTime)
+        private ButtonControl randomizeButton;
+        private OptionControl playPauseButton;
+        
+        private void InitializeComponent()
         {
-            KeyboardState newKeyboardState = Keyboard.GetState();
+            Bounds = new UniRectangle(0, 0, Automatone.SCREEN_WIDTH, Automatone.CONTROLS_AND_GRID_DIVISION);
+            Title = "Automatone";
 
-            if (ScrollWithMidi)
+            // Construct children
+            randomizeButton = new ButtonControl();
+            playPauseButton = new OptionControl();
+
+            //
+            // randomizeButton
+            //
+            randomizeButton.Bounds = new UniRectangle(10, 30, 117, 45);
+            randomizeButton.Text = "Randomize";
+            randomizeButton.Pressed += new EventHandler(RandomizeButtonPressed);
+
+            //
+            // playPauseButton
+            //
+            playPauseButton.Bounds = new UniRectangle(10, 75, 45, 42);
+            playPauseButton.Changed += new EventHandler(PlayPauseButtonChanged);
+
+            //
+            // Add Children
+            //
+            Children.Add(randomizeButton);
+            Children.Add(playPauseButton);
+        }
+
+        private void PlayPauseButtonChanged(object sender, EventArgs e)
+        {
+            if (playPauseButton.Selected)
             {
-                playOffset -= Automatone.TEMPO * Automatone.SUBBEATS_PER_MEASURE / 14400.0f * CELLSIZE;
-                gridOffset.X = Math.Min(playOffset + 100, 0);
+                automatone.Sequencer.PlayMidi();
             }
             else
             {
-                if (newKeyboardState.IsKeyDown(Keys.Left) == newKeyboardState.IsKeyDown(Keys.Right))
-                {
-                    moveValX = INIT_GRID_MOVE_SPEED;
-                }
-                else if (newKeyboardState.IsKeyDown(Keys.Left))
-                {
-                    moveValX += GRID_MOVE_ACCELERATION;
-                    if (moveValX > MAX_GRID_MOVE_SPEED)
-                    {
-                        moveValX = MAX_GRID_MOVE_SPEED;
-                    }
-
-                    gridOffset.X += moveValX;
-                }
-                else if (newKeyboardState.IsKeyDown(Keys.Right))
-                {
-                    moveValX += GRID_MOVE_ACCELERATION;
-                    if (moveValX > MAX_GRID_MOVE_SPEED)
-                    {
-                        moveValX = MAX_GRID_MOVE_SPEED;
-                    }
-
-                    gridOffset.X -= moveValX;
-                }
+                automatone.Sequencer.PauseMidi();
             }
 
-            if (newKeyboardState.IsKeyDown(Keys.Up) == newKeyboardState.IsKeyDown(Keys.Down))
-            {
-                moveValY = INIT_GRID_MOVE_SPEED;
-            }
-            else if (newKeyboardState.IsKeyDown(Keys.Up))
-            {
-                moveValY += GRID_MOVE_ACCELERATION;
-                if (moveValY > MAX_GRID_MOVE_SPEED)
-                {
-                    moveValY = MAX_GRID_MOVE_SPEED;
-                }
-
-                gridOffset.Y += moveValY;
-            }
-            else if (newKeyboardState.IsKeyDown(Keys.Down))
-            {
-                moveValY += GRID_MOVE_ACCELERATION;
-                if (moveValY > MAX_GRID_MOVE_SPEED)
-                {
-                    moveValY = MAX_GRID_MOVE_SPEED;
-                }
-
-                gridOffset.Y -= moveValY;
-            }
-            
-            gridOffset.X = MathHelper.Clamp(gridOffset.X, gridPanel.BoundingRectangle.Width - 10 - CELLSIZE - (gridWidth * CELLSIZE), 0);
-            gridOffset.Y = MathHelper.Clamp(gridOffset.Y, gridPanel.BoundingRectangle.Height - 160 - CELLSIZE - (gridHeight * CELLSIZE), 0);
-
-            HandleMouseInput(false);
-            gridPanel.Update(gameTime);
- 	        base.Update(gameTime);
+            automatone.GridScreen.ScrollWithMidi = playPauseButton.Selected;
         }
 
-        public override void Draw(GameTime gameTime)
+        private void RandomizeButtonPressed(object sender, EventArgs e)
         {
-            foreach (MSGUIClickable cell in gridPanel.ClickableComponents)
-            {
-                Vector2 drawPosition = cell.Position + gridOffset;
-                if (gridPanel.BoundingRectangle.Intersects(new Rectangle((int)drawPosition.X, (int)drawPosition.Y, CELLSIZE, CELLSIZE)))
-                {
-                    if (Math.Abs(cell.Position.X) < -1 * playOffset - CELLSIZE)
-                    {
-                        spriteBatch.Draw(cell.CollisionTexture, drawPosition, Color.Gray);
-                    }
-                    else if (Math.Abs(cell.Position.X) < -1 * playOffset)
-                    {
-                        spriteBatch.Draw(cell.CollisionTexture, drawPosition, Color.SpringGreen);
-                    }
-                    else
-                    {
-                        spriteBatch.Draw(cell.CollisionTexture, drawPosition, Color.White);
-                    }
-                }
-            }
+            automatone.Sequencer.StopMidi();
+            automatone.GridScreen.ScrollWithMidi = false;
 
-            base.Draw(gameTime);
-        }
+            StreamWriter sw = new StreamWriter("sample.mtx");
+            sw.WriteLine("MFile 1 2 192");
+            sw.WriteLine("MTrk");
+            sw.WriteLine("0 TimeSig 4/4 24 8");
+            sw.WriteLine("0 Tempo " + TEMPO_DIVIDEND / Automatone.TEMPO);
+            sw.WriteLine("0 KeySig 0 major");
+            sw.WriteLine("0 Meta TrkEnd");
+            sw.WriteLine("TrkEnd");
 
-        public void AddGridButton(MSButton button, int horizontalIndex, int verticalIndex)
-        {
-            gridPanel.AddComponent(button);
-            int buttonXPosition = gridPanel.BoundingRectangle.X + horizontalIndex * MainScreen.CELLSIZE;
-            int buttonYPosition = gridPanel.BoundingRectangle.Y + verticalIndex * MainScreen.CELLSIZE;
-            button.BoundingRectangle = new Rectangle(
-                                buttonXPosition,
-                                buttonYPosition,
-                                MainScreen.CELLSIZE, MainScreen.CELLSIZE);
-            gridAccessor.Add(new Vector2(buttonXPosition, buttonYPosition), button);
-        }
+            CellState[,] output;
+#if USESEED
+            String song = SongGenerator.GenerateSong(new Random(SEED), new ClassicalTheory(), out output);
+#else
+            String song = SongGenerator.GenerateSong(new Random(), new ClassicalTheory(), out automatone.SongCells);
+#endif
+            automatone.SongCells = output;
 
-        public void DestroyGridButtons()
-        {
-            gridPanel.ClickableComponents.Clear();
-            gridAccessor.Clear();
+            sw.Write(song);
+            sw.Close();
+
+            if (File.Exists("sample.mid"))
+                File.Delete("sample.mid");
+
+            Process txt2midi = new Process();
+            System.Console.WriteLine(Environment.CurrentDirectory);
+            txt2midi.StartInfo.WorkingDirectory = Environment.CurrentDirectory;
+            txt2midi.StartInfo.FileName = "Mtx2Midi.exe";
+            txt2midi.StartInfo.Arguments = "sample.mtx";
+            txt2midi.StartInfo.UseShellExecute = true;
+            txt2midi.Start();
+            automatone.Sequencer.LoadMidi("sample.mid");
+            txt2midi.Kill();
         }
     }
 }
